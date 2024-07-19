6a53c42fa7e17e7d56e4c69218964e9134f1e005
Trần Minh Thái <thaitranminh564@gmail.com>
2024-07-19 07:46:57 +0700

    init project

---
+++ Backend_Gundam_Ecommerce/src/main/java/com/backend_gundam_ecommerce/BackendGundamEcommerceApplication.java
@@ -0,0 +1,13 @@
+package com.backend_gundam_ecommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendGundamEcommerceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackendGundamEcommerceApplication.class, args);
+    }
+
+}
---
+++ Backend_Gundam_Ecommerce/src/test/java/com/backend_gundam_ecommerce/BackendGundamEcommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend_gundam_ecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendGundamEcommerceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
