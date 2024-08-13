ebf0a779866ae204abd55c0d9bd080b557f13387
Trần Minh Thái <thaitranminh564@gmail.com>
2024-08-14 03:16:36 +0700

    update header user

--- Frontend_Gundam_Ecommerce/src/layout/user/UserHeader.tsx
+++ Frontend_Gundam_Ecommerce/src/layout/user/UserHeader.tsx
@@ -1,12 +1,11 @@
-import { AppBar, Box, Drawer, IconButton, Menu, MenuItem, Stack, Toolbar } from '@mui/material'
-import React, { useState } from 'react'
-import { numberCartStyle } from '../../styles/styles'
+import { AppBar, BottomNavigation, BottomNavigationAction, Box, Container, IconButton, Menu, MenuItem, Toolbar, useMediaQuery } from '@mui/material';
+import React, { useState } from 'react';
+import { numberCartStyle } from '../../styles/styles';
 
 const UserHeader: React.FC = () => {
-
-    const [isLargeSize, setIsLargeSize] = useState<boolean>(true)
-
-    const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
+    const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
+    const [bottomNavValue, setBottomNavValue] = useState<number>(0);
+    const isLargeSize = useMediaQuery('(min-width: 900px)');
 
     const handleMenu = (event: React.MouseEvent<HTMLElement>) => {
         setAnchorEl(event.currentTarget);
@@ -17,38 +16,24 @@ const UserHeader: React.FC = () => {
     };
 
     return (
-        <>
-            <Box
-                position='sticky'
-                top={0}
-            >
-                <AppBar
-                    position='static'
-                    sx={{
-                        backgroundColor: 'white',
-                        boxShadow: 'none',
-                        borderBottom: '1px solid #b8b8b8'
-                    }}
-                >
-                    {isLargeSize
-                        ?
-                        <Toolbar >
-                            <Box
-                                display='flex'
-                                justifyContent='space-between'
-                                width='100%'
-                                mx={15}
+        <Box position='sticky' top={0} zIndex={1}>
+            <AppBar position='static' sx={{ backgroundColor: 'white', boxShadow: 'none', borderBottom: '1px solid #b8b8b8' }}>
+                <Toolbar>
+                    <Container maxWidth='xl' sx={{ display: 'flex', justifyContent: isLargeSize ? 'space-between' : 'center', alignItems: 'center' }}>
+                        {!isLargeSize && (
+                            <IconButton
+                                aria-label="menu"
+                                sx={{ fontSize: 30, color: '#1A2023' }}
                             >
-                                <Box>
-                                    <img src="/logo.png" alt="" height={45} />
-                                </Box>
-                                <Box>
-                                    <IconButton
-                                        sx={{
-                                            color: '#1A2023',
-                                            mr: 8
-                                        }}
-                                    >
+                                <img src="/logo.png" alt="" height={45} />
+                            </IconButton>
+                        )}
+
+                        {isLargeSize && (
+                            <>
+                                <img src="/logo.png" alt="Logo" height={45} />
+                                <Box sx={{ display: 'flex', alignItems: 'center' }}>
+                                    <IconButton sx={{ color: '#1A2023', mr: 8 }}>
                                         <i className='fa-solid fa-cart-shopping fs-2'
                                             style={{
                                                 position: 'relative'
@@ -61,57 +46,78 @@ const UserHeader: React.FC = () => {
                                             </Box>
                                         </i>
                                     </IconButton>
+
                                     <IconButton
                                         aria-label="account of current user"
                                         aria-controls="menu-appbar"
                                         aria-haspopup="true"
                                         onClick={handleMenu}
-                                        sx={{
-                                            color: '#1A2023'
-                                        }}
+                                        sx={{ color: '#1A2023' }}
                                     >
                                         <i className='fa-solid fa-user fs-2' />
                                     </IconButton>
-                                    <Menu
-                                        id="menu-appbar"
-                                        anchorEl={anchorEl}
-                                        anchorOrigin={{
-                                            vertical: 'top',
-                                            horizontal: 'left',
-                                        }}
-                                        keepMounted
-                                        transformOrigin={{
-                                            vertical: 'top',
-                                            horizontal: 'right',
-                                        }}
-                                        open={Boolean(anchorEl)}
-                                        onClose={handleClose}
-                                    >
-                                        <MenuItem onClick={handleClose}>Profile</MenuItem>
-                                        <MenuItem onClick={handleClose}>Logout</MenuItem>
-                                    </Menu>
                                 </Box>
-                            </Box>
-                        </Toolbar>
-                        :
-                        <Toolbar >
-                            <IconButton
-                                edge="start"
-                                aria-label="menu"
-                                sx={{
-                                    fontSize: 30,
-                                    mr: 2,
-                                    color: '#1A2023'
-                                }}
-                            >
-                                <i className="fa-solid fa-bars" />
-                            </IconButton>
-                        </Toolbar>
-                    }
-                </AppBar>
-            </Box >
+                            </>
+                        )}
+                    </Container>
+                </Toolbar>
+            </AppBar>
 
-        </>
+            {!isLargeSize && (
+                <BottomNavigation
+                    value={bottomNavValue}
+                    onChange={(_, newValue) => {
+                        setBottomNavValue(newValue);
+                    }}
+                    showLabels
+                    sx={{ position: 'fixed', bottom: 0, left: 0, right: 0, zIndex: 1, boxShadow: '0px -2px 10px rgba(0, 0, 0, 0.1)' }}
+                >
+                    <BottomNavigationAction
+                        label="Home"
+                        icon={<i className='fa-solid fa-house fs-2' />}
+                    />
+                    <BottomNavigationAction
+                        label="Cart"
+                        icon={
+                            <Box position='relative'>
+                                <i className='fa-solid fa-cart-shopping fs-2'
+                                    style={{
+                                        position: 'relative'
+                                    }}>
+                                    <Box
+                                        component='span'
+                                        sx={numberCartStyle}
+                                    >
+                                        5
+                                    </Box>
+                                </i>
+                                <Box component='span' sx={numberCartStyle}>5</Box>
+                            </Box>
+                        }
+                    />
+                    <BottomNavigationAction
+                        label="Account"
+                        aria-label="account of current user"
+                        aria-controls="menu-appbar"
+                        aria-haspopup="true"
+                        icon={<i className='fa-solid fa-user fs-2' />}
+                        onClick={handleMenu}
+                    />
+                </BottomNavigation>
+            )}
+            <Menu
+                id="menu-appbar"
+                anchorEl={anchorEl}
+                anchorOrigin={{ vertical: 'top', horizontal: 'right' }}
+                keepMounted
+                transformOrigin={{ vertical: 'top', horizontal: 'left' }}
+                open={Boolean(anchorEl)}
+                onClose={handleClose}
+            >
+                <MenuItem onClick={handleClose}>Profile</MenuItem>
+                <MenuItem onClick={handleClose}>Logout</MenuItem>
+            </Menu>
+        </Box>
     )
 }
 
